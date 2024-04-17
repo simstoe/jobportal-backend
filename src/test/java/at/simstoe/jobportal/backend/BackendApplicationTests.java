913216aa3594d2431c6c9f44062ff4fae2086475
stoegerer_simon <simon.stoegerer@htlpinkafeld.at>
2024-04-17 11:48:48 +0200

    Initial commit

---
+++ src/test/java/at/simstoe/jobportal/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.simstoe.jobportal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
